0282b9294f3e40bb7aeb65c077a6e1df15a00336
Nejimaki-Dori <sss63@mail.ru>
2020-12-10 18:30:34 +0400

    определился с датой по умолчанию в конструкторе Record

--- homework.py
+++ homework.py
@@ -3,9 +3,11 @@ import datetime as dt
 
 class Calculator:
     def __init__(self, limit):
-        records = []
+        self.records = []
+        self.limit = limit
 
-    def add_record(self, pl):
+    def add_record(self, record):
+        self.records.append(record)
         pass
 
     def get_today_stats(self, parameter_list):
@@ -20,19 +22,43 @@ class Calculator:
         """
         raise NotImplementedError    
     
-
-
 class Record:
-    def __init__(self, amount, comment, date=dt.datetime.today()):
+    def __init__(self, amount, comment, date=dt.datetime.today().strftime('%d.%m.%Y')):
         self.amount = amount
         self.comment = comment
-        self.date = date
+        self.date = dt.datetime.strptime(date, '%d.%m.%Y')
         
+class CaloriesCalculator(Calculator):
+    def __init__(self, limit):
+        super().__init__(limit)        
         
-        
+    def get_calories_remained():
+        pass
+
+# Сегодня можно съесть что-нибудь ещё, но с общей калорийностью не более N кКал», если лимит limit не достигнут,
+# или «Хватит есть!
+
+class CashCalculator(Calculator):
+    USD_RATE = 73.51
+    EUR_RATE = 89.14
+
+    def __init__(self, limit):
+        super().__init__(limit)
+
+    def get_today_cash_remained(currency):
+        pass
+        #«На сегодня осталось N руб/USD/Euro» — в случае, если лимит limit не достигнут,
+        #или «Денег нет, держись», если лимит достигнут,
+        #или «Денег нет, держись: твой долг - N руб/USD/Euro», 
+
+
+
+
+
+
 # для CashCalculator 
 r1 = Record(amount=145, comment="Безудержный шопинг")
-r2 = Record(amount=1568, comment="Наполнение потребительской корзины", date="09-03-2019")
+r2 = Record(amount=1568, comment="Наполнение потребительской корзины", date="09.03.2019")
 r3 = Record(amount=691, comment="Катание на такси", date="08.03.2019")
 
 # для CaloriesCalculator
